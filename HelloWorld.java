c941b02cf268b41c7381c7859406c676a25e1196
RobertCostner <rcostner@rboc.net>
2015-03-23 10:31:25 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld { 
+   public static void main(String[] args) { 
+      System.out.println("++++++++++");
+System.out.println("+ Hello, World.");
+System.out.println("+ This is a test of the Java Compiler.");
+System.out.println("+ Yea!");
+System.out.println("+++++++++++");
+   }
+}
